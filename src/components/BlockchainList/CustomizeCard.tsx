bc60fb87f953d52020a9ec06ac7441eb67ff8377
smyzin <smyzin.work@gmail.com>
2022-09-15 17:44:48 +0300

    Fix styles

--- src/components/BlockchainList/CustomizeCard.tsx
+++ src/components/BlockchainList/CustomizeCard.tsx
@@ -1,4 +1,4 @@
-import { AccentText, HeaderText, PostText } from 'components/ui/Text'
+import { AccentText, CardSubheader, PostText } from 'components/ui/Text'
 import Button from 'components/ui/Button'
 import Card from 'components/ui/Card'
 import classnames, {
@@ -12,28 +12,28 @@ const cardContainer = classnames(
   display('flex'),
   flexDirection('flex-col'),
   alignItems('items-start'),
-  gap('gap-y-4')
+  gap('gap-y-2')
 )
 export default function () {
   return (
     <Card blueBg>
       <div className={cardContainer}>
-        <HeaderText>
+        <CardSubheader>
           Customize your anonymous burner wallet with{' '}
           <AccentText bold color="text-accent">
             SealCred{' '}
           </AccentText>
-        </HeaderText>
-        <PostText>
+        </CardSubheader>
+        <PostText small>
           SealCred is an open-source privacy-preserving social protocol powered
           by zero-knowledge proofs. We allow users to easily create pseudonymous
           identities/wallets and transfer social capital from wallet A to wallet
           B using anonymous but verified, soulbound zkNFTs or zkBadges.
         </PostText>
-        <Button type="primary" small url="https://sealcred.xyz">
-          Try it out.
-        </Button>
       </div>
+      <Button type="primary" small url="https://sealcred.xyz">
+        Try it out.
+      </Button>
     </Card>
   )
 }
