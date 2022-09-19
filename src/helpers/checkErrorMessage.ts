9d02358c3708805c3679392a41421e1b88136522
smyzin <smyzin.work@gmail.com>
2022-09-19 22:10:17 +0300

    Add empty line

--- src/helpers/checkErrorMessage.ts
+++ src/helpers/checkErrorMessage.ts
@@ -4,6 +4,7 @@ export default function (error: unknown) {
   // we need to cast type to get real axios error message
   if (axios.isAxiosError(error) && error.response?.data)
     return (error.response.data as { [key: string]: unknown }).message ?? error
+
   if (error instanceof Error) return error.message
 
   return error
