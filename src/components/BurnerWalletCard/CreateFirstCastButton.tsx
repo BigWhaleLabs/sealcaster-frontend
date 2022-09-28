ad8fd7d71465a0590a95b14f7d7805b828f9e069
Michel Erokhov <mixaileroxov@gmail.com>
2022-09-28 16:36:49 +0500

    add used state for private key

--- src/components/BurnerWalletCard/CreateFirstCastButton.tsx
+++ src/components/BurnerWalletCard/CreateFirstCastButton.tsx
@@ -1,17 +1,21 @@
 import { Link } from 'wouter'
 import { displayFrom, displayTo } from 'helpers/visibilityClassnames'
+import { useSnapshot } from 'valtio'
+import BurnerWalletStore from 'stores/BurnerWalletStore'
 import Button from 'components/ui/Button'
 
 export default function () {
+  const { used } = useSnapshot(BurnerWalletStore)
+
   return (
     <Link href="/cast">
       <div className={displayTo('xs')}>
         <Button small type="primary">
-          Create your first cast
+          Create your {!used && 'first'} cast
         </Button>
       </div>
       <div className={displayFrom('xs')}>
-        <Button type="primary">Create your first cast</Button>
+        <Button type="primary">Create your {!used && 'first'} cast</Button>
       </div>
     </Link>
   )
