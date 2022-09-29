f2c059a7caa25b32c4dcfe34658183bdbe6b7159
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-09-29 17:09:30 +0400

    move to network

--- src/components/BlockchainList/EtherScanLink.tsx
+++ src/components/BlockchainList/EtherScanLink.tsx
@@ -1,5 +1,5 @@
 import { LinkText, StatusText } from 'components/ui/Text'
-import getEtherscanTxUrl from 'helpers/getEtherscanTxUrl'
+import getEtherscanTxUrl from 'helpers/network/getEtherscanTxUrl'
 
 export default function ({ tx }: { tx: string }) {
   return (
