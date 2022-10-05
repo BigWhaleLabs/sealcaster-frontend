1437cc276ff61d06de9b12d43f51189a2c9855ce
Daniil <dpancov64@gmail.com>
2022-10-05 15:32:40 +0300

    vertical dilimiter only

---
+++ src/components/ui/VerticalDelimiter.tsx
@@ -0,0 +1,32 @@
+import { Size, displayFrom } from 'helpers/visibilityClassnames'
+import { StatusText } from 'components/ui/Text'
+import classnames, {
+  TArg,
+  alignItems,
+  justifyContent,
+  width,
+} from 'classnames/tailwind'
+
+const bottomSeparator = classnames(
+  width('w-fit'),
+  alignItems('items-center'),
+  justifyContent('justify-center')
+)
+
+export default function ({
+  color = 'default',
+  className,
+  showFrom = 'xs',
+}: {
+  color?: 'accent' | 'primary' | 'error' | 'dimmed' | 'default'
+  className?: TArg
+  showFrom?: Size
+}) {
+  return (
+    <div
+      className={classnames(bottomSeparator, className, displayFrom(showFrom))}
+    >
+      <StatusText color={color}>|</StatusText>
+    </div>
+  )
+}
