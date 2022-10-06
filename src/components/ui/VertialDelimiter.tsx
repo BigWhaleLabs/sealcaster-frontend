3b26ddd222086b5b6e3acac81fa076244e19aba0
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-10-06 17:18:26 +0400

    fix vertial delimiter

--- src/components/ui/VertialDelimiter.tsx
+++ src/components/ui/VertialDelimiter.tsx
@@ -1,21 +1,5 @@
-import { Size, displayFrom } from 'helpers/visibilityClassnames'
-import { StatusText } from 'components/ui/Text'
-import classnames, {
-  TArg,
-  alignItems,
-  backgroundColor,
-  height,
-  justifyContent,
-  width,
-} from 'classnames/tailwind'
+import classnames, { backgroundColor, height, width } from 'classnames/tailwind'
 
-const bottomSeparator = classnames(
-  width('w-fit'),
-  alignItems('items-center'),
-  justifyContent('justify-center')
-)
-
-// TODO: make bg black
 const vertialDelimiter = classnames(
   height('h-px'),
   width('w-full'),
