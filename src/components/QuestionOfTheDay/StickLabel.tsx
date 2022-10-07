8d2d7ab3f8721986f862d8590a84742bdb2d621a
smyzin <smyzin.work@gmail.com>
2022-10-07 12:29:38 +0300

    Fix sticky label

--- src/components/QuestionOfTheDay/StickLabel.tsx
+++ src/components/QuestionOfTheDay/StickLabel.tsx
@@ -1,13 +1,21 @@
 import QuestionOfTheDayLabel from 'icons/QuestionOfTheDayLabel'
-import classnames, { inset, position } from 'classnames/tailwind'
+import classnames, {
+  dropShadow,
+  inset,
+  position,
+  transforms,
+} from 'classnames/tailwind'
 
 const badgeWrapper = (mobile?: boolean) =>
   classnames(
     position('absolute'),
     inset(
-      mobile ? '-right-12.5' : '-right-30',
-      mobile ? '-bottom-35' : '-top-12.5'
-    )
+      'xs:-right-2.5',
+      mobile ? 'body:-right-12.5' : 'body:-right-25',
+      mobile ? '-bottom-27.5' : '-top-2.5'
+    ),
+    transforms('-rotate-15'),
+    dropShadow('drop-shadow-accent-light-transparent')
   )
 
 export default function ({ mobile }: { mobile?: boolean }) {
