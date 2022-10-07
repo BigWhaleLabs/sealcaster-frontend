96230acb0022c21c02c971302da2daaf4b9d145a
smyzin <smyzin.work@gmail.com>
2022-10-07 13:07:45 +0300

    Add tilt effect

--- src/components/QuestionOfTheDay/StickLabel.tsx
+++ src/components/QuestionOfTheDay/StickLabel.tsx
@@ -1,4 +1,6 @@
+import { useRef } from 'preact/hooks'
 import QuestionOfTheDayLabel from 'icons/QuestionOfTheDayLabel'
+import Tilt from 'react-parallax-tilt'
 import classnames, {
   dropShadow,
   inset,
@@ -14,14 +16,32 @@ const badgeWrapper = (mobile?: boolean) =>
       mobile ? 'body:-right-12.5' : 'body:-right-25',
       mobile ? '-bottom-27.5' : '-top-2.5'
     ),
-    transforms('-rotate-15'),
-    dropShadow('drop-shadow-accent-light-transparent')
+    transforms('-rotate-15')
   )
+const imageFilter = (hueAngle: number) =>
+  `hue-rotate(${hueAngle}deg) drop-shadow(0px 0px 7rem var(--accent-light-transparent))`
 
 export default function ({ mobile }: { mobile?: boolean }) {
+  const badge = useRef<HTMLDivElement>(null)
+
   return (
     <div className={badgeWrapper(mobile)}>
-      <QuestionOfTheDayLabel mobile={mobile} />
+      <Tilt
+        glareEnable
+        glareMaxOpacity={0.4}
+        glareBorderRadius={'100%'}
+        tiltReverse
+        perspective={500}
+        onMove={({ tiltAngleX, tiltAngleY }) => {
+          if (!badge.current) return
+          badge.current.style.filter = imageFilter(tiltAngleX + tiltAngleY)
+        }}
+        className={dropShadow('drop-shadow-accent-light-transparent')}
+      >
+        <div ref={badge}>
+          <QuestionOfTheDayLabel mobile={mobile} />
+        </div>
+      </Tilt>
     </div>
   )
 }
