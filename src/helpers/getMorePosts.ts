9e09f02dc6e396bba7a3dd6411265905fa10cce4
Daniil <dpancov64@gmail.com>
2022-10-07 21:02:38 +0300

    removed unused vars

--- src/helpers/getMorePosts.ts
+++ src/helpers/getMorePosts.ts
@@ -4,23 +4,10 @@ import { handleError } from '@big-whale-labs/frontend-utils'
 import safeGetPostsAmountFromContract from 'helpers/safeGetPostsAmountFromContract'
 import safeGetThreadFromContract from 'helpers/safeGetThreadFromContract'
 
-export default async function ({
-  contract,
-  limitAmount,
-  loadedPostAmount = 0,
-}: {
-  contract: SCPostStorage
-  limitAmount: number
-  loadedPostAmount?: number
-}) {
+export default async function ({ contract }: { contract: SCPostStorage }) {
   try {
     const totalAmount = await safeGetPostsAmountFromContract(contract)
     if (totalAmount === 0) return Promise.resolve([])
-
-    const leftRecords = totalAmount - loadedPostAmount
-    const finalLimit = Math.min(limitAmount, leftRecords)
-    const shouldSkip = totalAmount - finalLimit - loadedPostAmount
-    const finalSkip = Math.max(0, shouldSkip)
 
     return safeGetThreadFromContract(0, contract)
   } catch (error) {
