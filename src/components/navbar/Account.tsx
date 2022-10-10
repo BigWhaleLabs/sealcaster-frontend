1bbef8bfde7afb2341b43ffe2de26084e5d534ee
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-10-10 15:36:29 +0400

    fix connecting wallet throw 404 error

--- src/components/navbar/Account.tsx
+++ src/components/navbar/Account.tsx
@@ -14,8 +14,6 @@ export default function ({
   needNetworkChange: boolean
   eNSName?: string
 }) {
-  const [location, setLocation] = useLocation()
-
   const NotConnected = () =>
     needNetworkChange ? (
       <span>Change network</span>
@@ -23,8 +21,6 @@ export default function ({
       <div
         onClick={async () => {
           await walletStore.connect(true)
-          if ((await walletStore.hasFarcasterBadge) && location !== '/cast')
-            setLocation('/cast')
         }}
       >
         <span className={displayTo('lg')}>No wallet</span>
