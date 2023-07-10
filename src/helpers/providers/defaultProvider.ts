b60c24f4cdad23b6ba5205817af89dc8d61b6c91
T_DaMER <dpancov64@gmail.com>
2023-07-10 18:50:51 +0300

    Use same rpc for heavy/default providers

--- src/helpers/providers/defaultProvider.ts
+++ src/helpers/providers/defaultProvider.ts
@@ -1,5 +1,7 @@
-import { ETH_RPC } from '@big-whale-labs/constants'
 import { providers } from 'ethers'
 import env from 'helpers/env'
 
-export default new providers.JsonRpcProvider(ETH_RPC, env.VITE_ETH_NETWORK)
+export default new providers.JsonRpcProvider(
+  env.VITE_ETH_RPC,
+  env.VITE_ETH_NETWORK
+)
